2084af53877bc5210380e1992b92ebc901659acd
Siddharth Thakor <sidht1295@gmail.com>
2018-05-03 18:42:44 -0400

    db created

--- app.js
+++ app.js
@@ -1,16 +1,41 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost/my_camp_site");
 
 app.use (bodyParser.urlencoded({extended: true}));
 app.set("view engine","ejs");
 
+//Schema Setup
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create(
+//     {
+//         name: "Blue Mountains",
+//         image:"https://smartcanucks.ca/wp-content/uploads/2016/11/blue-mountain-ontario-deals.jpg"
+//     }, function(err, campgorund) {
+//         if(err) {
+//             console.log(err);
+//         } else {
+//             console.log("New Campground created");
+//             console.log(campgorund);
+//         }
+//     }
+// )
+
 //array hardcoded
-var campgrounds = [
-    {name: "Niagara", image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8q8jVVqBU45uv83Ut_Mrbfw3vjeqRYoDz7Hc7Jfbi8Sk1g1Am"},
-    {name: "Blue Mountains", image:"https://smartcanucks.ca/wp-content/uploads/2016/11/blue-mountain-ontario-deals.jpg"},
-    {name: "Point Pelee", image:"https://s3.amazonaws.com/btoimage/prism-thumbnails/articles/201846-point-pelee-park.jpg-resize_then_crop-_frame_bg_color_FFF-h_1365-gravity_center-q_70-preserve_ratio_true-w_2048_.jpg"}
-];
+// var campgrounds = [
+//     {name: "Niagara", image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8q8jVVqBU45uv83Ut_Mrbfw3vjeqRYoDz7Hc7Jfbi8Sk1g1Am"},
+//     {name: "Blue Mountains", image:"https://smartcanucks.ca/wp-content/uploads/2016/11/blue-mountain-ontario-deals.jpg"},
+//     {name: "Point Pelee", image:"https://s3.amazonaws.com/btoimage/prism-thumbnails/articles/201846-point-pelee-park.jpg-resize_then_crop-_frame_bg_color_FFF-h_1365-gravity_center-q_70-preserve_ratio_true-w_2048_.jpg"}
+// ];
 
 //root
 app.get("/", function(req,res){
@@ -19,7 +44,14 @@ app.get("/", function(req,res){
 
 //campgrounds page
 app.get("/campgrounds", function(req,res){
-    res.render("campgrounds", {campgrounds: campgrounds});
+    //Get all campgrounds from DB
+    Campground.find({}, function(err, allCampgrounds){
+        if(err) {
+            console.log(err);
+        } else {
+            res.render("campgrounds", {campgrounds: allCampgrounds});
+        }
+    });
 });
 
 //creates new campground
@@ -28,9 +60,14 @@ app.post("/campgrounds", function(req,res){
     var name = req.body.name;
     var image = req.body.image;
     var  newCampground = {name: name, image: image}
-    campgrounds.push(newCampground);
-    //redirect to campgorund page
-    res.redirect("/campgrounds");
+    // create a new campground and save to DB
+    Campground.create(newCampground, function(err, newlyCreated){
+        if(err) {
+            console.log(err);
+        } else {
+            res.redirect("/campgrounds");
+        }
+    });
 });
 
 //send form values to post method above
